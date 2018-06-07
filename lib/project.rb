2a3086dad6e80db049adce252b25c56a51422f42
Megan Collier <meganraec@live.com>
2018-06-07 04:31:20 +0000

    automatically backed up by learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,10 +1,11 @@
 class Project
   attr_accessor :title, :backers
 
-  def initialize(title)
-    @title = title
+  def initialize#(title)
+    # @title = title
     @backers = []
   end
+
 
   def add_backer(added_backer)
     @backers << added_backer
